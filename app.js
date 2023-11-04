ec84ad067b4cdd6ae73d0d05d70e4a3052c1317a
davit khanal <khanaldavit@gmail.com>
2023-11-04 15:45:52 +0545

    connection string made for mongoDB database for wallet API

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const app = express();
+const mongo_connect = process.env.MONGO_URL;
 
+mongoose
+  .connect(mongo_connect, {})
+  .then(() => {
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  })
+  .catch((err) => {
+    console.log(" You failed to connect MongoDB!");
+  });
+
+
+// to listen the server in 8000
 app.listen(8000, () => {
   console.log("Server started at 8000");
 });
